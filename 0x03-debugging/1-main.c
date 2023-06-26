918ed3920a484743a46affc1f28d32b4f2f25216
VicHinga <vicmginga@gmail.com>
2023-06-26 15:53:06 -0700

    Pointers Arrays Strings

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -7,18 +8,17 @@ int main(void)
 {
         int i;
 
-        printf("Infinite loop incoming :(\n");
-
+	printf("Infinite loop incoming :(\n");
+	
         /**
         * i = 0;
-
+	
         * while (i < 10)
         * {
         *         putchar(i);
         * }
-        */
-        
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+        */	
+	printf("Infinite loop avoided! \\o/\n");
+	
+	return (0);
 }
