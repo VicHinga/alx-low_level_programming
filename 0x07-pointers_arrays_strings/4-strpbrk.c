4438c1177d7b0777bb6eb40443da8bf1ecdf2f79
VicHinga <vicmginga@gmail.com>
2023-07-04 13:08:27 -0700

    had to be betty compatible

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strspn - Entry point
+ * _strpbrk - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
@@ -19,5 +19,4 @@ char *_strpbrk(char *s, char *accept)
 		s++;
 	}
 	return ('\0');
-	
 }
